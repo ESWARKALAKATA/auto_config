65afce3895523de04d87aca9ef413c874584562b
eswar kalakata <eswar.kalakata@gmail.com>
2020-06-03 22:01:34 +0530

    making code excutable by removing some errors #second commit

--- auto_config.py
+++ auto_config.py
@@ -1,3 +1,9 @@
+'''
+pending : 
+apgroup3 neeed to update 
+need to add print stateent at every function call by checking configuration file
+taking input from excel
+'''
 #variables amd their values which are assumed entered by user
 user_input = 2
 #promting user for input vap1 as 1 and vap2 as 2 and vap3 as 3
@@ -5,172 +11,199 @@ user_vap1_input = 1
 user_vap2_input = 2
 user_vap3_input = 3
 user_input_lms_ip = "72.1.1.1"
-user_radius1_host_input = " 30.1.1.1"
+user_radius1_host_input = " 30.1.1.1" 
 user_radius1_key_input =  " qwerty"
 user_radius2_host_input =  "30.1.1.2"
 user_radius2_key_input =   "asdfgh"
-user_radius2_host_input =  "30.1.1.3"
-user_radius2_key_input =   "zxcvbn"
+user_radius3_host_input =  "30.1.1.3"
+user_radius3_key_input =   "zxcvbn"
 
 
 def main():
-    #user_input = int(input("please enter input to excute apgroups : "))
+    user_input = int(input("please enter input to excute apgroups : "))
+    if user_input == 1:
+        apgroup1(user_vap1_input,user_vap2_input)
+
     if user_input == 2:
-        #please enter virtual-ap print statement
+        #please enter virtual_ap print statement
         apgroup1(user_vap1_input,user_vap2_input)
         apgroup2()
     
+    if user_input == 3:
+        apgroup1(user_vap1_input,user_vap2_input)
+        apgroup2()
+        apgroup3()
+    
+        
 
 
 #apgroup1
 def apgroup1(user_vap1_input,user_vap2_input):
     if user_vap1_input == 1: #user_vap1_input = int(input("please enter "1" for vap1 : "))
-        wlan_virtual-ap_VAP-1()
+        wlan_virtual_ap_VAP_1()
     if user_vap2_input == 2: #user_vap2_input = int(input("please enter "2" for vap2: "))
-        wlan_virtual-ap_VAP-2()
-    APSYS() # ap-system-profile
-    MYRADIO-0() # dot11g-radio-profile
-    MYRADIO-1() # dot11g-radio-profile
-    TMP-1() #dot11a-traffic-mgmt-profile TMP-1
+        wlan_virtual_ap_VAP_2()
+    APSYS() # ap_system_profile
+    MYRADIO_0() # dot11g_radio_profile
+    MYRADIO_1() # dot11g_radio_profile
+    TMP_1() #dot11a_traffic_mgmt_profile TMP_1
 
 # group2
-def apgroup2(): #apgroup2 has only one option why should we ask user for input
-    wlan_virtual-ap_VAP-3()
-    APSYS() # ap-system-profile 
-    MYRADIO-0()
-    MYRADIO-1()
-    TMP-1()  #dot11a-traffic-mgmt-profile TMP-1
+def apgroup2():
+    #apgroup2 has only one option why should we ask user for input
+    wlan_virtual_ap_VAP_3()
+    APSYS() # ap_system_profile 
+    MYRADIO_0()
+    MYRADIO_1()
+    TMP_1()  #dot11a_traffic_mgmt_profile TMP_1
 
-            
+# group3
+def apgroup3():
+    print("still this block is pending neeed to update")
+    
 
 
-# virtual-aps
-def wlan_virtual-ap_VAP-1():
-    aaa-profile_AAA-1()
-    ssid-profile_SSID1()  
-    print("vap-enable")
+# virtual_aps
+def wlan_virtual_ap_VAP_1():
+    aaa_profile_AAA_1()
+    ssid_profile_SSID1()  
+    print("vap_enable")
     print("vlan 106")
 
-def wlan_virtual-ap_VAP-2():
-    aaa-profile_AAA-2()
-    ssid-profile_SSID2()
-    print("vap-enable")
+def wlan_virtual_ap_VAP_2():
+    aaa_profile_AAA_2()
+    ssid_profile_SSID2()
+    print("vap_enable")
     print("vlan 107")
     
 
-def wlan_virtual-ap_VAP-3():
-    aaa-profile_AAA-1()
-    ssid-profile_SSID3()
+def wlan_virtual_ap_VAP_3():
+    aaa_profile_AAA_1()
+    ssid_profile_SSID3()
     print("vlan 106")
 
-def APSYS(): # system-profile
-    print("syscontact "ABC")
+def APSYS(): # system_profile
+    print("syscontact : ABC")
     #user_input_lms_ip = str(input("Enter lms ip :"))
-    print("lms-ip %s",user_input_lms_ip)
+    print("lms_ip %s",user_input_lms_ip)
 
 
 
-#aaa-profiles
-def aaa-profile_AAA-1():
-    INIT-R1()
-    DOT1X-PF()
-    FINAL-R1()
-    SG-1()
+#aaa_profiles
+def aaa_profile_AAA_1():
+    INIT_R1()
+    DOT1X_PF()
+    FINAL_R1()
+    SG_1()
 
-def aaa-profile_AAA-2():
-    INIT-R2()
-    #authentication-mac MAC-2 "not mentioned discription line 57 in configuration file"
-    FINAL-R2()
-    SG-2()
-    rfc-3576-server 2.2.2.2()
+def aaa_profile_AAA_2():
+    INIT_R2()
+    #authentication_mac MAC_2 "not mentioned discription line 57 in configuration file"
+    FINAL_R2()
+    SG_2()
+    rfc_3576_server_2_2_2_2()
 
     
 #aaa fun reqirements
-def rfc-3576-server 2.2.2.2():
-    print("aaa rfc-3576-server 2.2.2.2")
+def rfc_3576_server_2_2_2_2():
+    print("aaa rfc_3576_server 2.2.2.2")
 
 
-def INIT-R1():#  initial-role
-    print("access-list session log-c") #user-role
-def INIT-R2()
-    print("captive-portal CP-SSID-2 ")
-    print("access-list session log-c")
+def INIT_R1():#  initial_role
+    print("access_list session log_c") #user_role
+def INIT_R2():
+    print("captive_portal CP_SSID_2 ")
+    print("access_list session log_c")
 
-def  DOT1X-PF(): #authentication
-    MAC-R1()
-    DOT1X-R1()
+def  DOT1X_PF(): #authentication
+    MAC_R1()
+    DOTIX_R1()
 
-def MAC-R1():# machine-authentication machine-default-role 
-    print("access-list session allowall")#user role
+def MAC_R1():# machine_authentication machine_default_role 
+    print("access_list session allowall")#user role
 
-def DOTIX-R1():# machine-authentication user-default-role
-    print("access-list session allowall")#user role     
+def DOTIX_R1():# machine_authentication user_default_role
+    print("access_list session allowall")#user role     
 
-def FINAL-R1():  #dot1x-default-role
-    print("access-list session allowall")#user role 
-def FINAL-R2():
-    print("access-list session ra-guard")
+def FINAL_R1():  #dot1x_default_role
+    print("access_list session allowall")#user role 
+def FINAL_R2():
+    print("access_list session ra_guard")
 
-def SG-1(): # dot1x-server-group
-    RADIUS-1()
-    RADIUS-2()
+def SG_1(): # dot1x_server_group
+    RADIUS_1()
+    RADIUS_2()
     
-def SG-2():
-    RADIUS-2()  
+def SG_2():
+    RADIUS_2()  
 #RADIUS
-def RADIUS-1():
-    #user_radius-1_host_input = input('enter_host_input :')
-    #user_radius-2_key_input = input('enter_host_input :')
-    print("host  %s", user_radius_host_input)
-    print("key  %s" ,user_radius_key_input)
+def RADIUS_1():
+    #user_radius_1_host_input = input('enter_host_input :')
+    #user_radius_2_key_input = input('enter_host_input :')
+    print("host  %s", user_radius1_host_input)
+    print("key  %s" ,user_radius1_key_input)
 
-def RADIUS-2():
-    #user_radius-2_host_input = input('enter_host_input :')
-    #user_radius-2_key_input = input('enter_host_input :')
-    print("host  %s", user_radius_host_input)
-    print("key  %s" ,user_radius_key_input)
+def RADIUS_2():
+    #user_radius_2_host_input = input('enter_host_input :')
+    #user_radius_2_key_input = input('enter_host_input :')
+    print("host  %s", user_radius2_host_input)
+    print("key  %s" ,user_radius2_key_input)
 
-def RADIUS-3():
-    #user_radius-3_host_input = input('enter_host_input :')
-    #user_radius-3_key_input = input('enter_host_input :')
-    print("host %s", user_radius_host_input)
-    print("key %s" ,user_radius_key_input)
+def RADIUS_3():
+    #user_radius_3_host_input = input('enter_host_input :')
+    #user_radius_3_key_input = input('enter_host_input :')
+    print("host %s", user_radius3_host_input)
+    print("key %s" ,user_radius3_key_input)
 
 
 
 #SSID'S
-def ssid-profile_SSID1():
+def ssid_profile_SSID1():
     #call wifi 1
-    essid "WIFI-1"
-    opmode wpa2-aes
+    print("essid WIFI_1")
+    #opmode wpa2_aes
 
-def ssid-profile_SSID2():
+def ssid_profile_SSID2():
     #call wifi 2
-    essid "WIFI-2"
+    print("essid WIFI_2")
 
-def ssid-profile_SSID3():
+def ssid_profile_SSID3():
+    print("essid WIFI_3")
     # calling wifi 3
 
 #wlans
-def MYRADIO-0():   # dot11g-radio-profile
-    MYARM-0()
-    MY-HT-0()
-    MY-AM-0()
-def MYRADIO-1():    # dot11g-radio-profile
-    MY-AM-1()
+def MYRADIO_0():   # dot11g_radio_profile
+    MYARM_0()
+    MY_HT_0()
+    MY_AM_0()
+def MYRADIO_1():    # dot11g_radio_profile
+    MYARM_1()
 
-def MYARM-0():   #rf arm-profile
-    print("40MHz-allowed-bands None")
+def MYARM_0():   #rf arm_profile
+    print("40MHz_allowed_bands None")
 
-def MY-HT-0():  #rf ht-radio-profile
-     print("ht-radio-profile")
+def MY_HT_0():  #rf ht_radio_profile
+     print("ht_radio_profile")
     
-def MY-AM-0(): #am-scan-profile
-     print("am-scan-profile")
+def MY_AM_0(): #am_scan_profile
+     print("am_scan_profile")
 
-def MYARM-1():  #rf arm-profile
-     print(" 40MHz-allowed-bands None ")
+def MYARM_1():  #rf arm_profile
+     print(" 40MHz_allowed_bands None ")
 
-def TMP-1(): #wlan traffic-management-profile
-    print("shaping-policy fair-access")
+def TMP_1(): #wlan traffic_management_profile
+    print("shaping_policy fair_access")
 
+#vlan
+
+def vlan():
+    vlan_num = int(input("Enter 1st vlan number : "))
+    vlan_num = int(input("Enter 2nd vlan number : "))
+    vlan_num = int(input("Enter 3rd vlan number : "))
+    vlan_num = int(input("Enter 4rd vlan number : "))
+
+
+
+
+if __name__ == "__main__":
+    main()
