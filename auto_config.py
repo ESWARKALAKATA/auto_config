50899de8194853adb6fc5def5b2cdf9cdb44100e
eswar kalakata <eswar.kalakata@gmail.com>
2020-06-04 14:33:04 +0530

    -vlan -vrrp -port are added to file -dhcp not done need some help

--- auto_config.py
+++ auto_config.py
@@ -4,6 +4,12 @@ apgroup3 neeed to update
 need to add print stateent at every function call by checking configuration file
 taking input from excel
 '''
+from openpyxl import load_workbook
+wb = load_workbook("questionnaire.xlsx")
+sheet = wb['Questions']
+
+
+
 #variables amd their values which are assumed entered by user
 user_input = 2
 #promting user for input vap1 as 1 and vap2 as 2 and vap3 as 3
@@ -195,15 +201,268 @@ def TMP_1(): #wlan traffic_management_profile
     print("shaping_policy fair_access")
 
 #vlan
+def main_vlan():
+    user_vlan_selction = int(input("Enter number of VLANs to be created : "))
+    if user_vlan_selction == 1:
+        vlan1()
+    if user_vlan_selction == 1:
+        vlan1()
+        vlan2()
+    if user_vlan_selction == 1:
+        vlan1()
+        vlan2()
+        vlan3()
+    if user_vlan_selction == 1:
+        vlan1()
+        vlan2()
+        vlan3()
+        vlan4()
+    
+def vlan1():
+    user_vlan = int(input("Enter 1st vlan number : "))
+    if user_vlan == 101:
+        print("vlan 101")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 101 IP address : "))
+        if user_ip == "101.1.1.1":
+            print("interface vlan 101")
+            print("ip address 101.1.1.2 255.255.255.0")
+            print("!")
+            vlan_name = str(input("Enter VLAN 101 name"))
+            if vlan_name == "AP Vlan":
+                print(' description "AP Vlan" ')
+                print("!")
 
-def vlan():
-    vlan_num = int(input("Enter 1st vlan number : "))
-    vlan_num = int(input("Enter 2nd vlan number : "))
-    vlan_num = int(input("Enter 3rd vlan number : "))
-    vlan_num = int(input("Enter 4rd vlan number : "))
+def vlan2():
+    user_vlan = int(input("Enter 2nd vlan number : "))
+    print("vlan 106")
+    if user_vlan == 106:
+        print("vlan 106")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 106 IP address : "))
+        if user_ip == "106.1.1.1":
+            print("interface gigabitethernet 0/0/1")
+            print("ip address 106.1.1.1 255.255.252.0")
+            print("!")
+            vlan_name = str(input("Enter VLAN 106 name"))
+            if vlan_name == "SSID1 Vlan":
+                print('  description "SSID1 Vlan" ')
+                print("!")
 
 
+        
+def vlan3():
+    user_vlan  = int(input("Enter 3rd vlan number : "))
+    if user_vlan == 107:
+        print("vlan 107")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 107 IP address : "))
+        if user_ip == "107.1.1.1":
+            print("interface vlan 107")
+            print(" ip address 107.1.1.1 255.255.240.0")
+            print("!")
+            vlan_name = str(input("Enter VLAN 107 name"))
+            if vlan_name == "SSID2 Vlan":
+                print('  description "SSID2 Vlan" ')
+                print("!")
+            
+           
 
 
+def vlan4():
+    user_vlan  = int(input("Enter 4rd vlan number : "))
+    if user_vlan == 500:
+        print("vlan 500")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 500 IP address : "))
+        if user_ip == "50.1.1.1":
+            print("interface vlan 500")
+            print('description "MGMT VLAN" ip address 50.1.1.1 255.255.255.0')
+            print("ip radius source-interface vlan 500")
+            print("ip radius source-interface vlan 500")
+            print("!")
+            vlan_name = str(input("Enter VLAN 500 name"))
+            if vlan_name == "MGMT VLAN":
+                print('  description  "MGMT VLAN" ')
+                print("!")
+'''              
+#DHCP not uderstandable
+def dhcp_main():
+    ser_req = str(input("Is DHCP service required :"))
+    if ser_req == "Yes":
+
+    if ser_req == "No":
+'''
+#vrrp 
+
+#vlan
+def main_vlan():
+    user_vlan_selction = int(input("Enter number of VLANs to be created : "))
+    if user_vlan_selction == 1:
+        vlan1()
+    if user_vlan_selction == 2:
+        vlan1()
+        vlan2()
+    if user_vlan_selction == 3:
+        vlan1()
+        vlan2()
+        vlan3()
+    if user_vlan_selction == 4:
+        vlan1()
+        vlan2()
+        vlan3()
+        vlan4()
+    
+def vlan1():
+    user_vlan = int(input("Enter 1st vlan number : "))
+    if user_vlan == 101:
+        print("vlan 101")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 101 IP address : "))
+        if user_ip == "101.1.1.1":
+            print("interface vlan 101")
+            print("ip address 101.1.1.2 255.255.255.0")
+            print("!")
+            vlan_name = str(input("Enter VLAN 101 name :"))
+            if vlan_name == "AP Vlan":
+                print(' description "AP Vlan" ')
+                print("!")
+
+def vlan2():
+    user_vlan = int(input("Enter 2nd vlan number : "))
+    print("vlan 106")
+    if user_vlan == 106:
+        print("vlan 106")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 106 IP address : "))
+        if user_ip == "106.1.1.1":
+            print("interface gigabitethernet 0/0/1")
+            print("ip address 106.1.1.1 255.255.252.0")
+            print("!")
+            vlan_name = str(input("Enter VLAN 106 name :"))
+            if vlan_name == "SSID1 Vlan":
+                print('  description "SSID1 Vlan" ')
+                print("!")
+
+
+        
+def vlan3():
+    user_vlan  = int(input("Enter 3rd vlan number : "))
+    if user_vlan == 107:
+        print("vlan 107")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 107 IP address : "))
+        if user_ip == "107.1.1.1":
+            print("interface vlan 107")
+            print(" ip address 107.1.1.1 255.255.240.0")
+            print("!")
+            vlan_name = str(input("Enter VLAN 107 name :"))
+            if vlan_name == "SSID2 Vlan":
+                print('  description "SSID2 Vlan" ')
+                print("!")
+            
+           
+
+def vlan4():
+    user_vlan  = int(input("Enter 4rd vlan number : "))
+    if user_vlan == 500:
+        print("vlan 500")
+        print("switchport trunk allowed vlan 500,101,106,107")
+        print("!")
+        user_ip = str(input("Enter VLAN 500 IP address : "))
+        if user_ip == "50.1.1.1":
+            print("interface vlan 500")
+            print('description "MGMT VLAN" ip address 50.1.1.1 255.255.255.0')
+            print("ip radius source-interface vlan 500")
+            print("ip radius source-interface vlan 500")
+            print("!")
+            vlan_name = str(input("Enter VLAN 500 name :"))
+            if vlan_name == "MGMT VLAN":
+                print('  description  "MGMT VLAN" ')
+                print("!")
+
+
+# VRRP_PORT FOR THIS INPUTS TAKEN FROM excel
+
+def vrrp_main():
+    VRRP_instance_number()
+    master_redundancy_password()
+    peer_IP_address()
+    VRRP_instance_name()
+    VRRP_IP_address()
+
+def VRRP_instance_number():
+    if str(sheet['D76'].value) == '101':
+        print("vrrp 101")
+    #if master redundancy password
+
+def master_redundancy_password():
+    if str(sheet['D77'].value) == 'vrrppassword':
+        print("authentication vrrppassword")
+
+def peer_IP_address():
+    if str(sheet['D78'].value) == '101.1.1.3':
+        print("peer-ip-address 101.1.1.3 ipsec masterpassword")
+def VRRP_instance_name():
+    if str(sheet['D79'].value) == 'Master':
+        print('description "Master"')
+
+def VRRP_IP_address():
+    if str(sheet['D80'].value) == '101.1.1.1':
+        print("ip address 101.1.1.1")
+        print("!")
+
+
+#port channel
+
+def port_main():
+    port_channeling_required()
+
+
+def port_channeling_required():
+    if str(sheet['D82'].value) == 'yes':
+        number_of_channels_required()
+        vlans_acess_permissions()
+        ports_added_to_port_chanel
+
+
+    if str(sheet['D82'].value) == 'no':
+        return
+        
+def number_of_channels_required():
+    if str(sheet['D83'].value) == '1':
+        print('interface port-channel 1')
+
+def vlans_acess_permissions():
+    if str(sheet['D84'].value) == 'all':
+        print(" switchport trunk allowed vlan 500,101,106,107")
+
+def ports_added_to_port_chanel(): 
+    if str(sheet['D85'].value) == '0/0/0, 0/0/1': 
+        print("lacp group 1 mode active")
+        print("lacp group 1 mode active")
+        print("interface gigabitethernet 0/0/0")
+        print("interface gigabitethernet 0/0/1")
+        print("!")  
+    
+
+             
 if __name__ == "__main__":
     main()
+    main_vlan()
+    vrrp_main()
+    port_main()
+    
+        
+
+
+
+    
+
